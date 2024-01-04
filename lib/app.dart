1a92131db7f6fc5e8e8cbb2f25ec1d2baf6528c9
Biruk Getachew <104642908+Barok-Getachew@users.noreply.github.com>
2024-01-04 22:55:56 +0300

    Update app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-
+import 'package:t_store/features/authentication/screens/login2/login_screen.dart';
 import 'package:t_store/features/authentication/screens/onboarding/onboarding.dart';
-
+import 'package:t_store/features/authentication/screens/signup/sign_up_screen.dart';
 import 'package:t_store/utils/constants/text_strings.dart';
 import 'package:t_store/utils/theme/theme.dart';
 
@@ -16,7 +16,9 @@ class App extends StatelessWidget {
       themeMode: ThemeMode.system,
       theme: TAppTheme.lightTheme,
       darkTheme: TAppTheme.darkTheme,
-      home: const OnBoardingScreen(),
+      // home: const OnBoardingScreen(),
+      home: SignupScreen(),
+      // home: LoginScreen(),
     );
   }
 }
