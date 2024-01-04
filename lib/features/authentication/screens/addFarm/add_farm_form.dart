6d19d9ad4af6e92df4a412ce5a0c9e6c8fc201f5
yaikobzeray <jac-2127>
2024-01-04 23:41:29 +0300

    ADDING ADD FARM FORM UI STATUS == DONE

---
+++ lib/features/authentication/screens/addFarm/add_farm_form.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
+import 'package:t_store/utils/constants/colors.dart';
+
+class addFarmForm extends StatelessWidget {
+  const addFarmForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Get.back();
+                  },
+                  child: Icon(Icons.arrow_back),
+                ),
+                const Text("Add Farm"),
+                Container(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 5, horizontal: 5),
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(212, 50, 121, 20),
+                      borderRadius: BorderRadius.circular(5)),
+                  child: const Text(
+                    'Save',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: size.height * 0.05,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: -10, horizontal: 10),
+                  label: Text('Farm Name'),
+                  labelStyle: TextStyle(color: TColors.darkGrey, fontSize: 14)),
+            ),
+            SizedBox(
+              height: size.height * 0.02,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: -10, horizontal: 10),
+                  label: Text('Location'),
+                  labelStyle: TextStyle(color: TColors.darkGrey, fontSize: 14)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
