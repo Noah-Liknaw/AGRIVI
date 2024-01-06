abb5a2b94592d71e13b77805bc32204f5cc3bc07
Noah-Liknaw <94854261+Noah-Liknaw@users.noreply.github.com>
2024-01-06 21:32:20 +0100

    ......

--- lib/features/Settings/screens/settings.dart
+++ lib/features/Settings/screens/settings.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
-class JOBS extends StatelessWidget {
-  const JOBS({super.key});
+class Settings extends StatelessWidget {
+  const Settings({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("JOBS")),
+      body: Center(child: Text("SETTINGS")),
     );
   }
 }
--- lib/features/authentication/controllers/login_form_controller.dart
+++ lib/features/authentication/controllers/login_form_controller.dart
@@ -2,7 +2,6 @@ import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:t_store/features/dashboard/screens/dashboard.dart';
 import 'package:t_store/features/fitness_app/fitness_app_home_screen.dart';
-import 'package:t_store/features/navigation/screens/dash/dash.dart';
 
 class LoginFormController extends GetxController {
   var isLoading = false.obs; // Observe the loading state
